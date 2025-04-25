40fa141c5efcc434194a6f7e0a19eb6771142370
cesarortizs <cgos2112@gmail.com>
2025-04-24 22:15:01 -0600

    Part 2 completed

---
+++ Part-2-Declaring-and-Initializing-Arrays/Part2/src/main/java/Part2/Main.java
@@ -0,0 +1,14 @@
+package Part2;
+
+public class Main {
+    public static void main(String[] args) {
+        // Declare and initialize an array of 5 double values
+        double[] doubleArray = {1, 2, 3, 4, 5};
+
+        // Assign values to each element and print them
+        for(int i = 0; i < doubleArray.length; i++)
+        {
+            System.out.println(doubleArray[i]);
+        }
+    }
+}
