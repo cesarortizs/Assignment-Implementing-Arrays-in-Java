0ade6db8123d06804403f53178425a36cdb8b0be
cesarortizs <cgos2112@gmail.com>
2025-04-24 22:02:30 -0600

    Part 1 completed

---
+++ Part-1-Understanding-Arrays/Part1/src/main/java/Part1/Main.java
@@ -0,0 +1,25 @@
+package Part1;
+
+public class Main {
+    public static void main(String[] args) {
+        // Declare an array of integers
+        int[] numbers;
+
+        // Initialize the array with 5 elements
+        numbers = new int[5];
+
+        // Access and assign values to array elements
+        numbers[0] = 10;
+
+        numbers[1] = 20;
+
+        numbers[2] = 30;
+
+        numbers[3] = 40;
+
+        numbers[4] = 50;
+
+        // Print the first element of the array
+        System.out.println(numbers[0]);
+    }
+}
